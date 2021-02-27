dd485e83574fbf013139750ab9aa75e4007f3b15
ZeinaAyman <56726381+ZeinaAyman@users.noreply.github.com>
2021-02-27 07:01:19 +0200

    fixed issue for instructor

--- fetch_user_chat_history.php
+++ fetch_user_chat_history.php
@@ -8,7 +8,7 @@ session_start();
 if ($_SESSION['type']==2 || ($_SESSION['type']==3) && isset($_POST['from'])) {
   echo fetch_user_chat_history($_POST['from'], $_POST['to_user_id'], $connect);
 }
-else if ($_SESSION['type']==1 || $_SESSION['type']== 4) {
+else if ($_SESSION['type']==1 || $_SESSION['type']== 4 || $_SESSION['type']== 5) {
   echo fetch_user_chat_history($_SESSION['id'], $_POST['to_user_id'], $connect);
 }
 
